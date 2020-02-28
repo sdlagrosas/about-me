71a78ce711ef150728c3d89f40d0acabfc23691c
sdlagrosas <sdlagrosas@up.edu.ph>
2020-02-28 11:11:58 +0800

    Added a MyName class for data binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -10,30 +10,34 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName:MyName = MyName( "Tim Hortons")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener{ addNickname() }
         binding.nicknameText.setOnClickListener { updateNickname() }
     }
 
     private fun addNickname() {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
 
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
 
-        binding.nicknameText.text = binding.nicknameEdit.text.toString()
-
-        binding.nicknameEdit.visibility = View.GONE
-        binding.doneButton.visibility = View.GONE
-
-        binding.nicknameText.visibility = View.VISIBLE
-
+            binding.nicknameText.visibility = View.VISIBLE
+        }
         // hide button
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
